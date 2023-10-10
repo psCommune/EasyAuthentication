61442647a199de4bc599731cd03ef8330d97dfd1
pscommune <daniil.mukhametov.96@mail.ru>
2023-10-10 17:15:21 +0500

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using EasyAuthentication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace EasyAuthentication.Controllers
+{
+    public class HomeController :Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController (ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            
+        }
+
+        public IActionResult Index ()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Privacy ()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Login (string username, string password)
+        {
+            // список утверждений о пользователе
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "Test User"),
+                new Claim(ClaimTypes.Role, "User"),
+                new Claim("TestValue", "Test")
+            };
+
+            // создаем "паспорт"
+            ClaimsIdentity identity = new ClaimsIdentity(claims,
+                CookieAuthenticationDefaults.AuthenticationScheme, // название схемы
+                ClaimTypes.Name, // название Claim, в котором записано имя
+                ClaimTypes.Role // название Claim, в котором записана роль
+             );
+            ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+            if (username == "admin" && password == "admin"){
+                // вход в систему: генерация и отправка cookie 
+                await HttpContext.SignInAsync(principal);
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Logout ()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error ()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(opt =>
+    {
+        opt.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+        opt.Cookie.Name = "cookie_test_app";
+        opt.LoginPath = "/"; // в случае отсутствия доступа перенаправим на главную
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();//добавил
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
